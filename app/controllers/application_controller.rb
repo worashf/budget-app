8e3672b861739efb7185516d5515b5199d985ddc
worashf <uppert83@gmail.com>
2022-10-12 23:41:48 +0300

    Configure allowed params for sign up and acount update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_sign_up_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_sign_up_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password current_password])
+  end
 end
