e18a04e77137bb384420c032140ea18224e3e143
worashf <uppert83@gmail.com>
2022-10-12 23:37:35 +0300

    Rename spenddings to spendings

--- app/models/spendding.rb
+++
@@ -1,5 +0,0 @@
-class Spendding < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :spendding_category, dependent: :destroy
-  has_many :categories, through: :spendding_category
-end
