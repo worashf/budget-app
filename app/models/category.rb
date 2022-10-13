75184342b0f6e76ba62aec95e5681549cf110fc4
worashf <uppert83@gmail.com>
2022-10-14 01:17:45 +0300

    Add category total  model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :spending_categories, dependent: :destroy
   has_many :spendings, through: :spending_categories
+
+  def category_total
+    spendings.sum(&:amount)
+  end
 end
