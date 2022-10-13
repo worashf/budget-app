fe3fb509c4fb9d384d7aaa759b42f92a0891c027
worashf <uppert83@gmail.com>
2022-10-14 01:22:06 +0300

    Add spendings and categoriesreource end-points

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "categories#index"
-  resources :categories
+  resources :categories , only: [:index, :create, :show, :destroy, :new] 
+  resources :spendings
 end
